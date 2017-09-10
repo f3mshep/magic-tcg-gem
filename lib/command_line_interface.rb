d9d0deae202ca0dcd40288b975488d77c05b30bc
Alexandra <superbiscuit@gmail.com>
2017-09-09 18:43:19 -0700

    working on CLI

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -5,5 +5,28 @@ require 'colorize'
 
 class CommandLine
 
+	def place_logo
+
+		puts "		                _            _____ _              ___      _   _               _             ".colorize.(:light_green)
+		puts "  /\\/\\   __ _  __ _(_) ___   _  /__   \\ |__   ___    / _ \\__ _| |_| |__   ___ _ __(_)_ __   __ _ ".colorize.(:light_green)
+		puts " /    \\ / _` |/ _` | |/ __| (_)   / /\\/ '_ \\ / _ \\  / /_\\/ _` | __| '_ \\ / _ \\ '__| | '_ \\ / _` |".colorize.(:light_green)
+		puts "/ /\\/\\ \\ (_| | (_| | | (__   _   / /  | | | |  __/ / /_\\\\ (_| | |_| | | |  __/ |  | | | | | (_| |".colorize.(:light_green)
+		puts "\\/    \\/\\__,_|\\__, |_|\\___| (_)  \\/   |_| |_|\\___| \\____/\\__,_|\\__|_| |_|\\___|_|  |_|_| |_|\\__, |".colorize.(:light_green)
+		puts "              |___/                                                                        |___/ ".colorize.(:light_green)
+		puts "                    ___              _     ___ _           _                                     ".colorize.(:light_green)
+		puts "                   / __\\__ _ _ __ __| |   / __(_)_ __   __| | ___ _ __                           ".colorize.(:light_green)
+		puts "                  / /  / _` | '__/ _` |  / _\\ | | '_ \\ / _` |/ _ \\ '__|                          ".colorize.(:light_green)
+		puts "                 / /__| (_| | | | (_| | / /   | | | | | (_| |  __/ |                             ".colorize.(:light_green)
+		puts "                 \\____/\\__,_|_|  \\__,_| \\/    |_|_| |_|\\__,_|\\___|_|                             ".colorize.(:light_green)
+	  
+	  puts ""                             
+
+	end
+
+	def get_input
+		puts "Please enter the name of a card you want to find:"
+		gets.strip
+	end
+
 
 end
