896f694d5127b4d1d99e769ed897c6d5c719716f
Alexandra <superbiscuit@gmail.com>
2017-09-11 15:25:46 -0700

    added spec file

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -12,25 +12,26 @@ class CommandLine
 
 	def start_screen
 		clear_screen
-		puts "		    _            _____ _              ___      _   _               _             ".colorize(:light_green)
-		puts "  /\\/\\   __ _  __ _(_) ___   _  /__   \\ |__   ___    / _ \\__ _| |_| |__   ___ _ __(_)_ __   __ _ ".colorize(:light_green)
-		puts " /    \\ / _` |/ _` | |/ __| (_)   / /\\/ '_ \\ / _ \\  / /_\\/ _` | __| '_ \\ / _ \\ '__| | '_ \\ / _` |".colorize(:light_green)
-		puts "/ /\\/\\ \\ (_| | (_| | | (__   _   / /  | | | |  __/ / /_\\\\ (_| | |_| | | |  __/ |  | | | | | (_| |".colorize(:light_green)
-		puts "\\/    \\/\\__,_|\\__, |_|\\___| (_)  \\/   |_| |_|\\___| \\____/\\__,_|\\__|_| |_|\\___|_|  |_|_| |_|\\__, |".colorize(:light_green)
-		puts "              |___/                                                                        |___/ ".colorize(:light_green)
-		puts "                    ___              _     ___ _           _                                     ".colorize(:light_green)
-		puts "                   / __\\__ _ _ __ __| |   / __(_)_ __   __| | ___ _ __                           ".colorize(:light_green)
-		puts "                  / /  / _` | '__/ _` |  / _\\ | | '_ \\ / _` |/ _ \\ '__|                          ".colorize(:light_green)
-		puts "                 / /__| (_| | | | (_| | / /   | | | | | (_| |  __/ |                             ".colorize(:light_green)
-		puts "                 \\____/\\__,_|_|  \\__,_| \\/    |_|_| |_|\\__,_|\\___|_|                             ".colorize(:light_green)
+		puts "				    _            _____ _              ___      _   _               _             ".colorize(:light_green)
+		puts "		  /\\/\\   __ _  __ _(_) ___   _  /__   \\ |__   ___    / _ \\__ _| |_| |__   ___ _ __(_)_ __   __ _ ".colorize(:light_green)
+		puts "		 /    \\ / _` |/ _` | |/ __| (_)   / /\\/ '_ \\ / _ \\  / /_\\/ _` | __| '_ \\ / _ \\ '__| | '_ \\ / _` |".colorize(:light_green)
+		puts "		/ /\\/\\ \\ (_| | (_| | | (__   _   / /  | | | |  __/ / /_\\\\ (_| | |_| | | |  __/ |  | | | | | (_| |".colorize(:light_green)
+		puts "		\\/    \\/\\__,_|\\__, |_|\\___| (_)  \\/   |_| |_|\\___| \\____/\\__,_|\\__|_| |_|\\___|_|  |_|_| |_|\\__, |".colorize(:light_green)
+		puts "		              |___/                                                                        |___/ ".colorize(:light_green)
+		puts "		                    ___              _     ___ _           _                                     ".colorize(:light_green)
+		puts "		                   / __\\__ _ _ __ __| |   / __(_)_ __   __| | ___ _ __                           ".colorize(:light_green)
+		puts "		                  / /  / _` | '__/ _` |  / _\\ | | '_ \\ / _` |/ _ \\ '__|                          ".colorize(:light_green)
+		puts "		                 / /__| (_| | | | (_| | / /   | | | | | (_| |  __/ |                             ".colorize(:light_green)
+		puts "		                 \\____/\\__,_|_|  \\__,_| \\/    |_|_| |_|\\__,_|\\___|_|                             ".colorize(:light_green)
 	  
 	  puts ""                             
-
+	  puts ""
+	  puts ""
 	  	run
 	end
 
 	def get_query
-		puts "Please enter the name of a card you want to find"
+		puts "				Please enter the name of a card you want to find"
 		gets.strip
 	end
 
---
+++ spec.md
@@ -0,0 +1,9 @@
+# Specifications for the CLI Assessment
+
+Specs:
+
+- [x] Have a CLI for interfacing with the application: Command line allows one to search cards, display a list of results, and access individual cards
+
+- [x] Pull data from an external source:Scrapes data from Scryfall.com's search page and card pages. 
+
+- [x] Implement both list and detail views: Search results display a list view, accessing a single card provides a detailed view of the card
